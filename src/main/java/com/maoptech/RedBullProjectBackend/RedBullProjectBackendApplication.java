534039e8b88fe13e1e60be51dd88356831c92363
Amin <Amin@PC>
2021-01-15 17:40:00 -0500

    Project Creation, Adding some properties in application.properties

---
+++ src/main/java/com/maoptech/RedBullProjectBackend/RedBullProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.maoptech.RedBullProjectBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedBullProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedBullProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maoptech/RedBullProjectBackend/RedBullProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maoptech.RedBullProjectBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedBullProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
